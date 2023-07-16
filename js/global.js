7f5dd44e6dc16b1caa4fd55f62cedcaa39cddeba
alangeek <alanchristiancreator@hotmail.com>
2023-07-16 14:29:46 -0300

    chore: Ripple effect

--- js/global.js
+++ js/global.js
@@ -1,6 +1,12 @@
 'use strict'
 
 /**
+ * Import
+ * **/
+
+import { ripple } from './utils/ripple.js'
+
+/**
  * Header on-scroll state
  */
 
@@ -9,3 +15,12 @@ const /** {NodeElelement}**/ $header = document.querySelector('[data-header]')
 window.addEventListener('scroll', () => {
   $header.classList[window.scrollY > 50 ? 'add' : 'remove']('active')
 })
+
+/**
+ * Add ripple effect
+ */
+
+const /** {NodeList} */ $rippleElems =
+    document.querySelectorAll('[data-ripple]')
+
+$rippleElems.forEach($rippleElem => ripple($rippleElem))
